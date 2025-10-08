bf094b6784698800e333e338b315676d89574f35
saba815 <sabunasesitashvili@gmail.com>
2025-10-08 13:06:55 -0700

    commit

---
+++ day 24/homework/lesson24.py
@@ -0,0 +1,18 @@
+def sum_three(num1, num2, num3):
+    return num1 + num2 + num3
+
+
+
+def user_info(name, age):
+    return f"{name} არის {age} წლის."
+
+
+
+def square(number):
+    return number ** 2
+
+
+
+print(sum_three(2, 4, 6))      
+print(user_info("ნიკა", 15)) 
+print(square(5))               
